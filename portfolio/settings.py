87d8d6f2c979c8bddfcbb56372d780892b0800e6
Rachit bedi <rachit.bedi1@gmail.com>
2019-03-01 12:29:57 +0530

    local settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,3 +132,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'portfolio/static/')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
